422827281fa8b78e99dcf9d8175f6905fce6eeb1
Udayan Tandon <Udayan12167@iiitd.ac.in>
2014-02-15 01:15:11 +0530

    Chap4 commit

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let (:base_title) {"FriendFundr"}
   describe "Home page" do
     it "should have the content 'Sample App" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -9,7 +10,7 @@ describe "StaticPages" do
     end
     it "should have title" do
       visit '/static_pages/home'
-      expect(page).to have_title("FriendFundr | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
    describe "Help page" do
@@ -19,7 +20,7 @@ describe "StaticPages" do
    	end
     it "should have title" do
       visit '/static_pages/help'
-      expect(page).to have_title("FriendFundr | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
   describe "About page" do
@@ -29,7 +30,17 @@ describe "StaticPages" do
     end
     it "should have title" do
       visit '/static_pages/about'
-      expect(page).to have_title("FriendFundr | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+  describe "Contact" do
+    it "should have title Contact" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+    it "should have content contact" do
+      visit '/static_pages/contact'
+      expect(page).to have_content("Contact")
     end
   end
 end
